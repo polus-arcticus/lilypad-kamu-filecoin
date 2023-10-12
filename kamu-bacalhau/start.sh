01141ee600d175ad3d0899d656f14774e0f3bf77
Taylor Hulsmans <thulsmans.1133@gmail.com>
2023-10-12 10:42:42 -0400

    kamu fastify debug

--- kamu-bacalhau/start.sh
+++ kamu-bacalhau/start.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+ipfs config Addresses.Gateway /ip4/0.0.0.0/tcp/8080
+ipfs config Addresses.API /ip4/0.0.0.0/tcp/5001
+ipfs init
+kamu init
 nohup ipfs daemon &
-nohup kamu system api-server --http-port 3080 --address 0.0.0.0
+nohup kamu system api-server --http-port 3080 --address 0.0.0.0 &
+nohup kamu ui --http-port 36083 --address 0.0.0.0 &
 npm run dev
