f4a1d38fde6d27b31b8f97aa2dd6ed7086a419b8
Taylor Hulsmans <thulsmans.1133@gmail.com>
2023-10-20 09:09:57 -0400

    add manifest

--- kamu-bacalhau/start.sh
+++ kamu-bacalhau/start.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
-#ipfs config Addresses.Gateway /ip4/0.0.0.0/tcp/8080
-#ipfs config Addresses.API /ip4/0.0.0.0/tcp/5001
-#ipfs init
+ipfs init
+ipfs config Addresses.Gateway /ip4/0.0.0.0/tcp/8080
+ipfs config Addresses.API /ip4/0.0.0.0/tcp/5001
 kamu init
-#kamu config set --user protocol.ipfs.httpGateway "http://0.0.0.0:8080"
+kamu config set --user protocol.ipfs.httpGateway "http://0.0.0.0:8080"
 
-#nohup ipfs daemon &
-
+nohup ipfs daemon &
+while true; do
+  if ipfs id >/dev/null 2>&1; then
+    echo "IPFS daemon is ready"
+    break
+  else
+    echo "Waiting for IPFS daemon to start..."
+    sleep 1
+  fi
+done
 kamu add ./test/ca.bankofcanada.exchange-rates.daily.yaml
 kamu system ipfs add ca.bankofcanada.exchange-rates.daily.yaml
 #nohup kamu system api-server --http-port 3080 --address 0.0.0.0 &
